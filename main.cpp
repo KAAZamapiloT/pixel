b00c1e71e6e0c09bcc6742a372fd7eae3c3d74d6
Kazamapilot <kazamapilot@gmail.com>
2026-01-02 02:14:37 +0530

    UPDATE:ADDING SDL3 IN IT

--- main.cpp
+++ main.cpp
@@ -3,10 +3,10 @@
 #include<cstdint>
 #include <memory>
 #include "Renderer.h"
+#include "SDL3/SDL.h"
 
 
-
-int main(){
+int main(int argc,char* argv){
 
     std::unique_ptr<renderer> r=std::make_unique<renderer>
     (1,std::make_unique<INV::Window>(512,512,"Main_Window"));
@@ -16,6 +16,27 @@ int main(){
     // modification loop
       float f=255.0/static_cast<float>(dims.x);
       float g=255.0/static_cast<float>(dims.y);
+
+      if(SDL_Init(SDL_INIT_VIDEO)!=0){
+          return -1;
+      }
+      SDL_Window* window = SDL_CreateWindow(
+           "SDL3 + MSVC",
+           dims.x, dims.y,
+           SDL_WINDOW_RESIZABLE
+       );
+       bool running = true;
+          SDL_Event e;
+
+          while (running) {
+              while (SDL_PollEvent(&e)) {
+                  if (e.type == SDL_EVENT_QUIT)
+                      running = false;
+              }
+          }
+
+          SDL_DestroyWindow(window);
+          SDL_Quit();
 /*
     for(uint16_t i=0;i<dims.y;i++){
         for(uint16_t j=0;j<dims.x;j++){
@@ -38,9 +59,10 @@ int main(){
    //r->DrawPolynomial([](float s){return std::log(s*s)*std::log(s*s*s);},S,U,C);
   //  r->DrawTriangle(S,U,INV::Vec2<uint16_t>(U.x,0),C,[](INV::Vec2<uint16_t> o,float norm){
       //         return INV::Vec3<uint8_t>(o.x*norm,o.y*norm,128);});
-    camera<float> cam(Type::Perspective,INV::Vec3<float>(0.0,0.0,0.0),60.0,dims.y/dims.x,0.001,50.0);
-    r->Draw_Tiangle_3d(cam,INV::Vec3<float>(S.x,S.y,3),INV::Vec3<float>(U.x,U.y,3),INV::Vec3<float>(0,U.y,3),C,nullptr);
-    r->Create_PPM_File("output.ppm");
+    //camera<float> cam(Type::Perspective,INV::Vec3<float>(0.0,0.0,0.0),60.0,dims.y/dims.x,0.001,50.0);
+   // r->Draw_Tiangle_3d(cam,INV::Vec3<float>(S.x,S.y,3),INV::Vec3<float>(U.x,U.y,3),INV::Vec3<float>(0,U.y,3),C,nullptr);
+ //   r->Create_PPM_File("output.ppm");
+
 
     std::cout << "Hello World!" << std::endl;
     return 0;
