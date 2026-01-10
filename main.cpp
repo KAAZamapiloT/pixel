68a2e70e37a25062ff4fc0087df0aeb72f4771c0
Kazamapilot <kazamapilot@gmail.com>
2026-01-10 23:43:56 +0530

    ISSUE:DONT KNOW WHEAT IS HAPPENIGN TO FRAME BUFFER AND WHY SCREEN IS BLACK?

--- main.cpp
+++ main.cpp
@@ -45,22 +45,24 @@ int main(int argc, char* argv[])
     INV::Vec2<double> A(45,45);
      INV::Vec2<double> B(100,100);
      INV::Vec2<double> C(150,150);
-INV::Vec3<uint8_t> w_color(1,1,1);
-INV::Vec3<uint8_t> col=(78,72,159);
-    while (running) {
+     INV::Vec3<uint8_t> w_color(1,1,121);
+     INV::Vec3<uint8_t> col=(178,72,1);
+     while (running) {
         while (SDL_PollEvent(&e)) {
             if (e.type == SDL_EVENT_QUIT)
                 running = false;
         }
 
         r->ClearColor(INV::Vec4<uint8_t>(w_color, 255));
-        r->DrawTriangle(A, B, C, col);
 
         SDL_UpdateTexture(texture, nullptr, pixels, pitch);
         SDL_RenderClear(sdlRenderer);
+        r->DrawTriangle(A, B, C, col);
+
         SDL_RenderTexture(sdlRenderer, texture, nullptr, nullptr);
         SDL_RenderPresent(sdlRenderer);
-    }
+        }
+
 
     SDL_DestroyTexture(texture);
     SDL_DestroyRenderer(sdlRenderer);
